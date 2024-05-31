adeef73df858f1433e2d6beaad5f91cfb2c5ec43
Alana Barbosa <itsalanayall@hotmail.com>
2024-05-31 12:35:10 -0300

    Exercicios programação estruturada

---
+++ Aula_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Mostrando mensagem com printf\n");
+
+    printf("Valor inteiro: %d.\n", 10);
+    printf("Valor real: %.2f.\n", 3.14159265);
+    printf("Dado de texto: %c. \n", 'a');
+    printf("Dado de texto: %s.\n", "testando");
+}
